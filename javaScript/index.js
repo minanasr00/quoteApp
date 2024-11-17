259463ad9763ce78186c0278c1651c78ceadc360
minanasr00 <62456457+minanasr00@users.noreply.github.com>
2024-11-17 17:45:37 +0200

    Update index.js

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,2 +1,32 @@
+var quote = document.getElementById("quote");
+var author = document.getElementById("author");
+var quotes = [{
+    quote: "Be yourself; everyone else is already taken.",
+    author : 'Oscar Wilde'
+}, {
+    quote: "Be the change that you wish to see in the world.",
+    author : 'Mahatma Gandhi'
+    }, {
+    quote: "If you tell the truth, you don't have to remember anything.",
+    author : "Mark Twain"
+    }, {
+    quote: "You only live once, but if you do it right, once is enough.",
+    author : "Mae West"
+    }, {
+    quote : "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+    author : "Mahatma Gandhi" 
+    }]
+
+var memory
+function addQuote() {
+    var Qindex = Math.floor(Math.random() * 5)
+    if (Qindex==memory) {
+        addQuote()
+    } else {
+        memory=Qindex
+        quote.innerText = quotes[Qindex].quote;        
+        author.innerText = quotes[Qindex].author;        
+    }
+}
 
 
